05c2728ef76cb9f201ce9aba5021af0042152214
AndresLFernandez <88495167+AndresLFernandez@users.noreply.github.com>
2022-09-18 16:54:22 -0300

    Agrego gitignore y actualizo mensajes

--- Entregable-clase-1/index.js
+++ Entregable-clase-1/index.js
@@ -2,12 +2,16 @@
 exports.__esModule = true;
 // Importo la libreria
 var fs = require("fs");
-// alojo en la variable texto el contenido del archivo como un string
+// Alojo en la variable texto el contenido del archivo numeros.txt como una cadena de string
 var texto = fs.readFileSync("numeros.txt", "utf-8");
-//let texto : string = "1 2 3 4 5"
-// alojo en la variable palabras arreglo de string caa una de las palabras
+// Alojo en el arreglo de string valores una cadena de caracteres por posicion que representa un numero
 var valores = texto.split(" ");
+// Alojo en el arreglo arrOfNum cada uno de los valores en formato Number
 var arrOfNum = valores.map(function (str) { return Number(str); });
+/* Funcion sumaArrego
+*  (in) Ingresa arreglo de numeros
+*  (out) Retorna un valor numerico que es la suma
+*/
 var sumarArreglo = function (arreglo) {
     var suma = 0;
     for (var i = 0; i < arreglo.length; i++) {
@@ -15,4 +19,5 @@ var sumarArreglo = function (arreglo) {
     }
     return suma;
 };
+//muestro por consola el resultado total
 console.log(sumarArreglo(arrOfNum));
--- Entregable-clase-1/index.ts
+++ Entregable-clase-1/index.ts
@@ -1,12 +1,16 @@
 // Importo la libreria
 import * as fs from 'fs';
-// alojo en la variable texto el contenido del archivo como un string
+// Alojo en la variable texto el contenido del archivo numeros.txt como una cadena de string
 let texto : string = fs.readFileSync("numeros.txt", "utf-8");
-//let texto : string = "1 2 3 4 5"
-// alojo en la variable palabras arreglo de string caa una de las palabras
+// Alojo en el arreglo de string valores una cadena de caracteres por posicion que representa un numero
 let valores : string[] = texto.split(" ");
+// Alojo en el arreglo arrOfNum cada uno de los valores en formato Number
 let arrOfNum = valores.map(str => { return Number(str); });
 
+/* Funcion sumaArrego 
+*  (in) Ingresa arreglo de numeros 
+*  (out) Retorna un valor numerico que es la suma
+*/
 const sumarArreglo = (arreglo: number[]): number => {
     let suma : number = 0; 
     for (let i: number = 0; i < arreglo.length; i++){
@@ -14,5 +18,5 @@ const sumarArreglo = (arreglo: number[]): number => {
     }
     return suma;
 }
-
+//muestro por consola el resultado total
 console.log(sumarArreglo(arrOfNum));
